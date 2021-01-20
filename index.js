b4923f2fec8212496ccaab03dff01454a7f980df
danyichingli <danyichingli@gmail.com>
2021-01-20 16:31:59 -0500

    second commit

--- index.js
+++ index.js
@@ -17,17 +17,24 @@ app.get("/", function(req,res){
 
 app.post("/", function(req,res){
   const symbol = req.body.symbol;
+  if (symbol )
   fs.readFile('secrets/api-key.txt', 'utf8', function(err, data){
-    if (err) { throw err; }
     const api_key = data;
     const url = "https://sandbox.iexapis.com/stable/stock/" + symbol + "/quote/latestPrice?token=" + api_key;
-
     https.get(url, function(response){
       response.on("data", function(latestPrice){
-        res.send(symbol.toUpperCase() + "'s Latest Price: "+ JSON.parse(latestPrice));
+        try{
+          res.send(symbol.toUpperCase() + "'s Latest Price: "+ JSON.parse(latestPrice));
+        } catch (e) {
+          res.sendFile(__dirname + "/failure.html");
+        }
       });
     });
   });
+});
+
+app.post("/failure", function(req, res){
+  res.redirect("/");
 });
 
 app.listen(process.env.PORT || 3000, function(){
